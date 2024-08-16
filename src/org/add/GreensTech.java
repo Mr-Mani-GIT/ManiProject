998b91d03a12d5cff0774cb81ac8b911604f3028
Mr-Mani-GIT <manin6891@gmail.com>
2024-08-16 21:08:14 +0530

    Master

--- src/org/add/GreensTech.java
+++ src/org/add/GreensTech.java
@@ -4,23 +4,12 @@ public class GreensTech {
 	public void greensOmr(String a) {
 		System.out.println("Location:"+a);
 	}
-public void greensOmr(int a) {
-	System.out.println("Pincode:"+a);
-	}
-public void greensOmr(String v,int d) {
-	System.out.println("Chennai:"+v+"\tOmr Id:"+d);
-}
-public void greensOmr(int d,String v) {
-	System.out.println("Number:"+d+"Name:"+v);
 
-}
 
 	public static void main(String[] args) {
 		GreensTech g = new GreensTech();
-		g.greensOmr(25663);
-		g.greensOmr("channai");
-		g.greensOmr(8657357, "Greens Technology");
-		g.greensOmr("location", 645);
+		g.greensOmr("Chennai");
+	
 	}
 
 }
